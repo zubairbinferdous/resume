31b3dd9d7e3b7b76990c33d2e9a25fec0e2dd6a3
zubairbinferdous <zubairbinf@gmail.com>
2022-02-17 03:09:07 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -4,19 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header ">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='name'>
+          Edit <code>Zubair </code> And Save Web Application Developer.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className='color style'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, ipsum aperiam? Ipsa cupiditate esse fugit quasi delectus alias iste  expedita atque doloremque autem similique quo, enim debitis! Laborum iure possimus minima quisquam delectus.</p>
+        <a className="App-link" href="https://reactjs.org"target="_blank"rel="noopener noreferrer" id='buttton'>Learn React Zubair bin ferdous </a>
       </header>
     </div>
   );
